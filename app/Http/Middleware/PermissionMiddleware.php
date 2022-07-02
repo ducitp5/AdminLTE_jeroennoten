35bd0730ea9b222df7b55e218f24e035de82df96
ductt <ducitp5@gmail.com>
2022-07-02 12:19:42 +0700

    attach hasrole to user

--- app/Http/Middleware/PermissionMiddleware.php
+++ app/Http/Middleware/PermissionMiddleware.php
@@ -1,10 +1,10 @@
 <?php
-
 namespace App\Http\Middleware;
-
+use App\Models\User;
 use Closure;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use Spatie\Permission\Exceptions\UnauthorizedException;
 class PermissionMiddleware
 {
     /**
@@ -14,8 +14,31 @@ class PermissionMiddleware
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle(Request $request, Closure $next)
+    public function handle($request, Closure $next, $permission = null, $guard = null)
     {
-        return $next($request);
+        $authGuard = app('auth')->guard($guard);
+        if ($authGuard->guest()) {
+            throw UnauthorizedException::notLoggedIn();
+        }
+        if (! is_null($permission)) {
+            $permissions = is_array($permission)
+                ? $permission
+                : explode('|', $permission);
+        }
+        if ( is_null($permission) ) {
+            $permission = $request->route()->getName();
+            $permissions = array($permission);
+        }
+        // nếu có toàn quyền
+        if(Auth::user()->can('all'))
+        {
+            return  $next($request);
+        }
+        foreach ($permissions as $permission) {
+            if (Auth::user()->can($permission)) {
+                return $next($request);
+            }
+        }
+        throw UnauthorizedException::forPermissions($permissions);
     }
 }
